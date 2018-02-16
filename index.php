6d03cec572e8c4d9d06b973af1e1028232a300d2
demrich <demrich@byjasco.com>
2018-02-16 16:40:20 -0600

    jquery and image progress

--- index.php
+++ index.php
@@ -11,17 +11,14 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
-
-    
 </head>
-
 <body>
     <header>
         <nav>
             <div class="container">
                 <div class="logo">
-                    LOGO GOES HERE
-                </div>
+              <a href="/"> <img src="./img/logo.png"> </a>            
+                </div>s
                 <div class="menu">
                     <ul>
                         <li>Purchase</li>
@@ -53,27 +50,31 @@
         <div id="colors">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
-                    <img src='https://dummyimage.com/500.gif' alt='' />
+                    <div class="image-box">
+                    <img id="cord-cover" src="https://dummyimage.com/500.gif" class="img-fluid" alt="" />
+                    </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-10 offset-md-1">
                     <ul>
+
                         <li>
-                            <img src='https://dummyimage.com/150.gif' alt='' />
+                            <div class="hover one" data-src="http://loremflickr.com/g/501/501/test" alt=''> </div>
                         </li>
                         <li>
-                            <img src='https://dummyimage.com/150.gif' alt='' />
+                            <div class="hover two" data-src="http://loremflickr.com/g/497/501/test" alt='' ></div>
                         </li>
                         <li>
-                            <img src='https://dummyimage.com/150.gif' alt='' />
+                            <div class="hover three" data-src="http://loremflickr.com/g/498/501/test" alt=''></div>
                         </li>
                         <li>
-                            <img src='https://dummyimage.com/150.gif' alt='' />
+                            <div class="hover four" data-src="http://loremflickr.com/g/499/501/test"></div>
                         </li>
                         <li>
-                            <img src='https://dummyimage.com/150.gif' alt='' />
+                            <div class="hover five" data-src="http://loremflickr.com/g/500/501/test" alt=''></div>
                         </li>
+
                     </ul><br>
                     <h1>Available in 5 Colors</h1>
                     <a class="btn btn-primary">Shop Now</a>
@@ -118,12 +119,10 @@
     </footer>
 
         <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
             crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
             crossorigin="anonymous"></script>
+        <script src="./js/custom.js"></script>
 </body>
 </html>
---
+++ js/custom.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+
+$('.hover').hover(function(){
+    var value=$(this).attr('data-src');
+    $('#cord-cover').attr('src', value).toggleClass('fade-in');
+
+});
+
+});
